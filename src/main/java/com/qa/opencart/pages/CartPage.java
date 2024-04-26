e691d80c9ad039743180ebd2ed39c89035451192
Bhanu Shukla <bhanushukla99@gmail.com>
2024-04-27 00:58:01 +0530

    cart page updated

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -1,12 +1,20 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class CartPage {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		
 		int i=10;
+		
+		private By name = By.id("test");
+		
+		public void getName() {
+			System.out.println("name");
+			
+		}
 
 	}
 
-}
+
